6ad18f3ea07b34e549e5aa7d997ddbb6f367cb10
Fiona Klute <fiona.klute@gmx.de>
2020-07-13 18:53:26 +0200

    Configurable server port

--- testserver/server.go
+++ testserver/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,9 +12,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "page.gohtml", r.Header)
 }
 
+var port = flag.Int("port", 8080, "server port to use")
 var templates = template.Must(template.ParseFiles("page.gohtml"))
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
