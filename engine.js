42c4d7cda7b0df793169d7aca6b5f571dd89e5b4
Fiona Klute <fiona.klute@gmx.de>
2022-08-12 23:26:44 +0200

    Create class to handle rules

--- engine.js
+++ engine.js
@@ -1,6 +1,6 @@
 /*
  * Referer Modifier: Modify the Referer header in HTTP requests
- * Copyright (C) 2017-2021 Fiona Klute
+ * Copyright (C) 2017-2022 Fiona Klute
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,79 @@
  */
 "use strict";
 /* exported RefererModEngine */
+
+class Rule
+{
+	// regular expression for the target domain
+	#target;
+	// action to take for matching requests
+	#action;
+	// Referer to set for "replace" rule
+	#referer;
+
+	constructor(rule)
+	{
+		/*
+		 * A rule is expected to have this structure in the stored
+		 * configuration:
+		 *
+		 * { domain: "www.example.com", action: "keep", referer: "" }
+		 */
+		this.#target = Rule.#createDomainRegex(rule.domain);
+		this.#action = rule.action;
+		this.#referer = rule.referer;
+	}
+
+	/*
+	 * Match "target" against this rule, return match object. Target
+	 * must be a URL object.
+	 */
+	match(target)
+	{
+		return target.hostname.match(this.#target);
+	}
+
+	get action()
+	{
+		return this.#action;
+	}
+
+	get referer()
+	{
+		return this.#referer;
+	}
+
+	/*
+	 * Escape function from
+	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+	 */
+	static #escapeRegExp(string)
+	{
+		// eslint-disable-next-line no-useless-escape
+		return string.replace(/[.*+?^${}()|\[\]\\]/g, "\\$&");
+		// $& means the whole matched string
+	}
+
+	/*
+	 * Create regular expressions from domains to also match subdomains.
+	 */
+	static #createDomainRegex(domain)
+	{
+		let pattern;
+		if (domain.endsWith("$"))
+		{
+			pattern = domain;
+		}
+		else
+		{
+			pattern = "(\\.|^)"
+				+ Rule.#escapeRegExp(domain) + "$";
+		}
+		//console.log(`domain '${domain.domain}', pattern: ${pattern}`);
+		return new RegExp(pattern);
+	}
+}
+
 
 class RefererModEngine
 {
@@ -42,19 +115,9 @@ class RefererModEngine
 
 	setConfig(config)
 	{
-		/*
-		* Configuration for specific domains
-		*
-		* Elements are expected to have this structure in the stored
-		* configuration:
-		*
-		* { domain: "www.example.com", action: "keep", referer: "" }
-		*
-		* While loading the configuration, a "regexp" field will be added that
-		* contains the regular expression compiled form the "domain" field to
-		* include subdomains.
-		*/
-		this.#domains = RefererModEngine.createDomainRegex(config.domains);
+		/* Configuration for specific domains. See Rule constructor
+		 * for the expected structure. */
+		this.#domains = config.domains.map(d => new Rule(d));
 		/* Configuration for same domain requests */
 		this.#sameConf = config.sameConf;
 		/* Configuration for any other requests */
@@ -73,7 +136,7 @@ class RefererModEngine
 		* (most precise) match in case we have multiple filter
 		* matches. */
 		let match = this.#domains
-			.map(d => ({domain: d, match: target.hostname.match(d.regexp)}))
+			.map(d => ({domain: d, match: d.match(target)}))
 			.filter(d => d.match != null)
 			.reduce((acc, current) =>
 			{
@@ -151,39 +214,5 @@ class RefererModEngine
 				referrer = originUrl;
 		}
 		return referrer;
-	}
-
-	/*
-	* Escape function from
-	* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-	*/
-	static escapeRegExp(string)
-	{
-		// eslint-disable-next-line no-useless-escape
-		return string.replace(/[.*+?^${}()|\[\]\\]/g, "\\$&");
-		// $& means the whole matched string
-	}
-
-	/*
-	* Create regular expressions from domains to also match subdomains.
-	*/
-	static createDomainRegex(domains)
-	{
-		for (let domain of domains)
-		{
-			let pattern;
-			if (domain.domain.endsWith("$"))
-			{
-				pattern = domain.domain;
-			}
-			else
-			{
-				pattern = "(\\.|^)"
-					+ RefererModEngine.escapeRegExp(domain.domain) + "$";
-			}
-			//console.log(`domain '${domain.domain}', pattern: ${pattern}`);
-			domain.regexp = new RegExp(pattern);
-		}
-		return domains;
 	}
 }
